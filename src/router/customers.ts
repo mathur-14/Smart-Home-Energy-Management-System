c79ce768671f1fc9071246951bcc0fdadc561370
Yash Mathur <ym2977@nyu.edu>
2023-12-07 03:54:48 -0500

    [FIX] customer api

--- src/router/customers.ts
+++ src/router/customers.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import * as Customer from '../controllers/customers'
 
 export default (router: express.Router) => {
-  router.post('/customer', Customer.createCustomer);
-  router.get('/customers', Customer.getAllCustomers);
-  router.get('/customer/:c_id', Customer.getCustomer);
-  router.put('/customer/:c_id', Customer.updateCustomer);
-  router.delete('/customer/:c_id', Customer.deleteCustomer);
-  router.put('/customer/:c_id/pwd-reset', Customer.resetCustomerPassword);
+  router.post('/v1/customer', Customer.createCustomer);
+  router.get('/v1/customers', Customer.getAllCustomers);
+  router.get('/v1/customer/:c_id', Customer.getCustomer);
+  router.put('/v1/customer/:c_id', Customer.updateCustomer);
+  router.delete('/v1/customer/:c_id', Customer.deleteCustomer);
+  router.put('/v1/customer/:c_id/pwd-reset', Customer.resetCustomerPassword);
 };
