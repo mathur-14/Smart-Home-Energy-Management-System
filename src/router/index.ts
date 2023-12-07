d19d58a3dfb8b2ffadbaa5016923132926feb065
Yash Mathur <ym2977@nyu.edu>
2023-12-07 07:02:56 -0500

    Add enroll devices

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import customers from './customers';
 import deviceModels from './device-models';
 import serviceLocations from './service-locations';
+import enrollDevices from './enroll-devices';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ export default (): express.Router => {
   customers(router);
   deviceModels(router);
   serviceLocations(router);
+  enrollDevices(router);
   return router;
 };
